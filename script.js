a9bb91befc173e8d695c5c48590a5646ba12b83f
Bendik Kodehode <bendik08.kodehode@gmail.com>
2024-02-10 22:51:36 +0100

    Added glowup when keypress and click

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 const container = document.querySelector("#container");
 const triggerBoxes = document.querySelectorAll(".triggers");
 
-// const hotKeys = {
-//     clap:    "q",
-//     hihat:   "w",
-//     kick:    "e",
-//     openhat: "r",
-//     ride:    "a",
-//     snare:   "s",
-//     tink:    "d",
-//     tom:     "f"
-// };
-
 const hotKeys = {
     q: "clap",
     w: "hihat",
@@ -25,26 +14,49 @@ const hotKeys = {
 
 for (let key in hotKeys) {
     const drumItem = document.createElement("div");
+    const drumName = document.createElement("p");
+    const drumHotkey = document.createElement("div");
     const drumKey = document.createElement("kbd");
 
-    drumItem.classList.add("item");
+    drumItem.classList.add("drum-item");
+    drumHotkey.classList.add("hotkey");
+    drumName.textContent = hotKeys[key].charAt(0).toUpperCase() + hotKeys[key].slice(1);
     drumKey.textContent = `${key}`;
 
-    container.append(drumItem);
-    drumItem.append(drumKey);
+    console.log(hotKeys[key])
 
-    drumItem.addEventListener("click", () => playSound(key));
+    drumHotkey.append(drumKey);
+    drumItem.append(drumName, drumHotkey);
+    container.append(drumItem);
+
+    drumHotkey.addEventListener("click", () => {
+        playSound(key);
+        drumGlow(key);
+    });
 };
-window.addEventListener("keypress", (event) => playSound(event.key))
+
+window.addEventListener("keypress", (event) => {
+    playSound(event.key);
+    drumGlow(event.key)
+});
 
 function playSound(key) {
     if (hotKeys[key]) new Audio(`./sounds/${hotKeys[key]}.wav`).play();
-    }
+};
 
 triggerBoxes.forEach(trigger => {
     trigger.addEventListener("click", () => {
         const soundName = trigger.id.split("-")[0];
         const soundKey = Object.keys(hotKeys).find(key => hotKeys[key] === soundName);
         playSound(soundKey); 
-    })
+    });
 });
+
+function drumGlow(key) {
+    triggerBoxes.forEach(trigger => {
+        if (trigger.id.split("-")[0] === hotKeys[key]) {
+            trigger.classList.add("drum-glow-up");
+            setTimeout(() => trigger.classList.remove("drum-glow-up"), 200);
+        };
+    });
+};
